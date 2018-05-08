233fe2fba7b8bfae5ed8abe65be2dac10c6e44aa
root <zqyangchn@gmail.com>
2018-05-08 11:57:16 +0800

    kubernetes v1.10.2

--- docker_image_master.sh
+++ docker_image_master.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
-#docker pull rewind/etcd-amd64:3.1.12
-#docker tag rewind/etcd-amd64:3.1.12 k8s.gcr.io/etcd-amd64:3.1.12
-#docker pull rewind/etcd-amd64:3.3.4
-#docker tag rewind/etcd-amd64:3.3.4 k8s.gcr.io/etcd-amd64:3.3.4
+etcd_version=3.1.12
+kubernetes_version=v1.10.2
+flannel_version=v0.10.0
+kube_pause_version=3.1
+
+kube_dns_version=1.14.10
+kube_dashboard_version=v1.8.3
+kube_heapster_version=v1.5.2
+kube_heapster_grafana_version=v4.4.3
+kube_heapster_influxdb_version=v1.3.3
+
+docker pull rewind/etcd-amd64:$etcd_version
+#docker tag rewind/etcd-amd64:$etcd_version k8s.gcr.io/etcd-amd64:$etcd_version
 
 
-#docker pull rewind/kube-apiserver-amd64:v1.10.0
-#docker pull rewind/kube-scheduler-amd64:v1.10.0
-#docker pull rewind/kube-controller-manager-amd64:v1.10.0
-#docker pull rewind/pause-amd64:3.1
-#docker pull rewind/kube-proxy-amd64:v1.10.0
-#docker pull rewind/flannel:v0.10.0-amd64
+docker pull rewind/kube-apiserver-amd64:$kubernetes_version
+docker pull rewind/kube-scheduler-amd64:$kubernetes_version
+docker pull rewind/kube-controller-manager-amd64:$kubernetes_version
+#docker pull rewind/pause-amd64:$kube_pause_version
+#docker pull rewind/kube-proxy-amd64:$kubernetes_version
+#docker pull rewind/flannel:$flannel_version-amd64
 
-#docker tag rewind/kube-apiserver-amd64:v1.10.0 k8s.gcr.io/kube-apiserver-amd64:v1.10.0
-#docker tag rewind/kube-scheduler-amd64:v1.10.0 k8s.gcr.io/kube-scheduler-amd64:v1.10.0
-#docker tag rewind/kube-controller-manager-amd64:v1.10.0 k8s.gcr.io/kube-controller-manager-amd64:v1.10.0
-#docker tag rewind/pause-amd64:3.1 k8s.gcr.io/pause-amd64:3.1
-#docker tag rewind/kube-proxy-amd64:v1.10.0 k8s.gcr.io/kube-proxy-amd64:v1.10.0
-#docker tag rewind/flannel:v0.10.0-amd64 quay.io/coreos/flannel:v0.10.0-amd64
+#docker tag rewind/kube-apiserver-amd64:$kubernetes_version k8s.gcr.io/kube-apiserver-amd64:$kubernetes_version
+#docker tag rewind/kube-scheduler-amd64:$kubernetes_version k8s.gcr.io/kube-scheduler-amd64:$kubernetes_version
+#docker tag rewind/kube-controller-manager-amd64:$kubernetes_version k8s.gcr.io/kube-controller-manager-amd64:$kubernetes_version
+#docker tag rewind/pause-amd64:$kube_pause_version k8s.gcr.io/pause-amd64:$kube_pause_version
+#docker tag rewind/kube-proxy-amd64:$kubernetes_version k8s.gcr.io/kube-proxy-amd64:$kubernetes_version
+#docker tag rewind/flannel:$flannel_version-amd64 quay.io/coreos/flannel:$flannel_version-amd64
--- docker_image_node.sh
+++ docker_image_node.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
 
-#docker pull rewind/kube-proxy-amd64:v1.10.0
-#docker tag rewind/kube-proxy-amd64:v1.10.0 k8s.gcr.io/kube-proxy-amd64:v1.10.0
+kubernetes_version=v1.10.2
+flannel_version=v0.10.0
+kube_pause_version=3.1
 
-#docker pull rewind/flannel:v0.10.0-amd64
-#docker tag rewind/flannel:v0.10.0-amd64 quay.io/coreos/flannel:v0.10.0-amd64
+kube_dns_version=1.14.10
+kube_dashboard_version=v1.8.3
+kube_heapster_version=v1.5.2
+kube_heapster_grafana_version=v4.4.3
+kube_heapster_influxdb_version=v1.3.3
 
-docker pull rewind/pause-amd64:3.1
-docker tag rewind/pause-amd64:3.1 k8s.gcr.io/pause-amd64:3.1
+#docker pull rewind/kube-proxy-amd64:$kubernetes_version
+#docker tag rewind/kube-proxy-amd64:$kubernetes_version k8s.gcr.io/kube-proxy-amd64:$kubernetes_version
+#
+#docker pull rewind/flannel:$flannel_version-amd64
+#docker tag rewind/flannel:$flannel_version-amd64 quay.io/coreos/flannel:$flannel_version-amd64
+
+docker pull rewind/pause-amd64:$kube_pause_version
+docker tag rewind/pause-amd64:$kube_pause_version k8s.gcr.io/pause-amd64:$kube_pause_version
 
 # kube-dns image
-docker pull rewind/k8s-dns-kube-dns-amd64:1.14.8
-docker pull rewind/k8s-dns-dnsmasq-nanny-amd64:1.14.8
-docker pull rewind/k8s-dns-sidecar-amd64:1.14.8
-docker tag rewind/k8s-dns-kube-dns-amd64:1.14.8 k8s.gcr.io/k8s-dns-kube-dns-amd64:1.14.8
-docker tag rewind/k8s-dns-dnsmasq-nanny-amd64:1.14.8 k8s.gcr.io/k8s-dns-dnsmasq-nanny-amd64:1.14.8
-docker tag rewind/k8s-dns-sidecar-amd64:1.14.8 k8s.gcr.io/k8s-dns-sidecar-amd64:1.14.8
+docker pull rewind/k8s-dns-kube-dns-amd64:$kube_dns_version
+docker tag rewind/k8s-dns-kube-dns-amd64:$kube_dns_version k8s.gcr.io/k8s-dns-kube-dns-amd64:$kube_dns_version
+docker pull rewind/k8s-dns-dnsmasq-nanny-amd64:$kube_dns_version
+docker tag rewind/k8s-dns-dnsmasq-nanny-amd64:$kube_dns_version k8s.gcr.io/k8s-dns-dnsmasq-nanny-amd64:$kube_dns_version
+docker pull rewind/k8s-dns-sidecar-amd64:$kube_dns_version
+docker tag rewind/k8s-dns-sidecar-amd64:$kube_dns_version k8s.gcr.io/k8s-dns-sidecar-amd64:$kube_dns_version
 
 # Dashboard image
-docker pull rewind/kubernetes-dashboard-amd64:v1.8.3
-docker tag rewind/kubernetes-dashboard-amd64:v1.8.3 k8s.gcr.io/kubernetes-dashboard-amd64:v1.8.3
+docker pull rewind/kubernetes-dashboard-amd64:$kube_dashboard_version
+docker tag rewind/kubernetes-dashboard-amd64:$kube_dashboard_version k8s.gcr.io/kubernetes-dashboard-amd64:$kube_dashboard_version
 
 # heapster image
-docker pull rewind/heapster-amd64:v1.5.2
-docker pull rewind/heapster-grafana-amd64:v4.4.3
-docker pull rewind/heapster-influxdb-amd64:v1.3.3
-docker tag rewind/heapster-amd64:v1.5.2 gcr.io/google_containers/heapster-amd64:v1.5.2
-docker tag rewind/heapster-grafana-amd64:v4.4.3 gcr.io/google_containers/heapster-grafana-amd64:v4.4.3
-docker tag rewind/heapster-influxdb-amd64:v1.3.3 gcr.io/google_containers/heapster-influxdb-amd64:v1.3.3
+docker pull rewind/heapster-amd64:$kube_heapster_version
+docker tag rewind/heapster-amd64:$kube_heapster_version gcr.io/google_containers/heapster-amd64:$kube_heapster_version
+docker pull rewind/heapster-grafana-amd64:$kube_heapster_grafana_version
+docker tag rewind/heapster-grafana-amd64:$kube_heapster_grafana_version gcr.io/google_containers/heapster-grafana-amd64:$kube_heapster_grafana_version
+docker pull rewind/heapster-influxdb-amd64:$kube_heapster_influxdb_version
+docker tag rewind/heapster-influxdb-amd64:$kube_heapster_influxdb_version gcr.io/google_containers/heapster-influxdb-amd64:$kube_heapster_influxdb_version
 
